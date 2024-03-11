ec5de8acdc47af66c86b1b72079d0c1337055bf4
fameteor <artus.francois@laposte.net>
2024-03-11 18:07:00 +0100

    V1.0.1

--- application/myApp/app.js
+++ application/myApp/app.js
@@ -11,6 +11,122 @@ let citiesList = {};
 // -----------------------------------------------
 // Global functions
 // -----------------------------------------------
+const addPlace = function() {
+
+	// We check if data is OK ----------------------
+	const isDecimal = function(text) {
+		const decimal = /^-?(0|[1-9]\d*)(\.\d+)?$/;
+		return decimal.test(text);
+	}
+	const name = $("#name").val().trim();
+	let lat = $("#lat").val().trim();
+	let long = $("#long").val().trim();
+
+	let error = false;
+	let errorMsgs = {
+	};
+
+	// The name must be filled
+	if (!name) {
+		$("#name").addClass('error')
+		error = true;
+		errorMsgs.name = "le nom est obligatoire";
+	}
+	else {
+		$("#name").removeClass('error');
+	}
+
+	// The lat must be filled, decimal and between -90 and 90
+	if (!lat) {
+		$("#lat").addClass('error');
+		error = true;
+	}
+	else {
+		if (isDecimal(lat)) {
+			lat = parseFloat(lat);
+			if (lat >= -90 && lat <= 90) {
+				$("#lat").removeClass('error');
+			}
+			else {
+				$("#lat").addClass('error');
+				error = true;
+			}
+		}
+		else {
+			$("#lat").addClass('error');
+			error = true;
+		}
+	}
+
+	// The long must be filled, decimal and between -180 and 180
+	if (!long) {
+		$("#long").addClass('error');
+		error = true;
+	}
+	else {
+		if (isDecimal(long)) {
+			long = parseFloat(long);
+			if (long >= -180 && long <= 180) {
+				$("#long").removeClass('error');
+			}
+			else {
+				$("#long").addClass('error');
+				error = true;
+			}
+		}
+		else {
+			$("#long").addClass('error');
+			error = true;
+		}
+	}
+
+	// We check if everything is ok ----------------
+	if (!error) {
+		// Data is ok, we save and display -----------
+		KAI.config.cities.push({
+			location: 					String(lat) + ',' + String(long),
+			choiceList_label: 	name,
+			choiceList_type:		"BOOLEAN"
+		});
+		citiesList.currentIndex = citiesList.list.length -1;
+		// We save the configuration -----------------
+		KAI.SD.saveConfig();
+		// We clear the form -------------------------
+		$("#name").val('');
+		$("#lat").val('');
+		$("#long").val('');
+		// We display a message and go to meteo_page1
+		KAI.toastr.info("Nouveau lieu ajouté");
+		KAI.newState('meteo_page1');
+	}
+	else {
+		KAI.toastr.warning("Merci de bien remplir correctement tous les champs")
+	}
+}
+
+const getCurrentPosition = function() {
+	KAI.spinner.on("Attente de la position GPS actuelle...");
+	that = this;
+	window.navigator.geolocation.getCurrentPosition(
+		function(position) {
+			KAI.spinner.off();
+			if (position.coords.accuracy < 50) {
+				$("#lat").val(position.coords.latitude);
+				$("#long").val(position.coords.longitude);
+			}
+			else {
+				KAI.toastr.warning("Position GPS imprécise, réessayer avec une meilleur couverture satellite")
+			}
+			console.log(position);
+		},
+		function(error) {
+			KAI.spinner.off();
+			KAI.toastr.warning("Erreur GPS : " + error.message);
+		}
+	);
+};
+
+
 const nextDay = function() {
 	if (dayIndex < (forecastData.length -1)) dayIndex +=1;
 }
@@ -38,6 +154,9 @@ const displayMeteoForSelectedCity = function() {
       KAI.spinner.off();
       KAI.toastr.warning(err.statusText);
 			console.log(err.statusText);
+			forecastData = [];
+			$("#meteo_page1_data").html('météo non disponible');
+	    $("#meteo_page2_data").html('météo non disponible');
     });
 }
 
@@ -70,18 +189,11 @@ KAI.addState("meteo_page1", {
     'div#meteo_page1' : true,
 		'div#meteo_page2' : false,
     'div#citiesList': false,
-    'div#addCity': false,
+    'div#addPlace': false,
     'div#actions': false
   },
   afterStateChange : function() {
-
-		/*
-    dictionnaries.generateHtml();
-    // After 200 ms we select the searchWord input field
-    setTimeout(() => {
-      document.getElementById("searchWord").select();
-    }, 200);
-		*/
+		displayMeteoForSelectedCity();
   },
 	events : {
 		'keyup.ArrowDown': function(event) {
@@ -101,6 +213,9 @@ KAI.addState("meteo_page1", {
 			previousDay();
 			KAI.renderSoftKeys();
 			meteo_render(forecastData);
+		},
+		'keyup.Enter': function(event) {
+			KAI.newState('addPlace');
 		},
     'keyup.SoftRight': function(event) {
 			nextDay();
@@ -127,7 +242,7 @@ KAI.addState("meteo_page2", {
     'div#meteo_page1' : false,
 		'div#meteo_page2' : true,
     'div#citiesList': false,
-    'div#addCity': false,
+    'div#addPlace': false,
     'div#actions': false
   },
   afterStateChange : function() {
@@ -158,10 +273,74 @@ KAI.addState("meteo_page2", {
 			KAI.renderSoftKeys();
 			meteo_render(forecastData);
 		},
+		'keyup.Enter': function(event) {
+			KAI.newState('addPlace');
+		},
     'keyup.SoftRight': function(event) {
 			nextDay();
 			KAI.renderSoftKeys();
 			meteo_render(forecastData);
+		},
+    'window.focus': function(event) {
+      KAI.warmStart();
+		}
+	}
+});
+
+// -----------------------------------------------
+// States meteo_page2
+// -----------------------------------------------
+KAI.addState("addPlace", {
+  softKeys : {fr : [
+		'GPS',
+		'annuler',
+		'ajouter'
+	]},
+  display : {
+		'div#meteo' : false,
+    'div#meteo_page1' : false,
+		'div#meteo_page2' : false,
+    'div#citiesList': false,
+    'div#addPlace': true,
+    'div#actions': false
+  },
+  afterStateChange : function() {
+    // After 200 ms we select the name input field
+    setTimeout(() => {
+      document.getElementById("name").select();
+    }, 200);
+  },
+	events : {
+		'keyup.ArrowUp': function(event) {
+			const previousToFocusList = $(event.target).prevAll('.focusable');
+			if (previousToFocusList.length >= 1) {
+				// We select the previous input class if not first
+				previousToFocusList.first().select();
+			}
+			else {
+				// We select the last input
+				$('.focusable').last().select();
+			}
+		},
+		'keyup.ArrowDown': function(event) {
+			const nextToFocusList = $(event.target).nextAll('.focusable');
+			if (nextToFocusList.length >= 1) {
+				// We select the next input class if not last
+				nextToFocusList.first().select();
+			}
+			else {
+				// We select the first input
+				$('.focusable').first().select();
+			}
+		},
+		'keyup.SoftLeft': function(event) {
+			getCurrentPosition();
+		},
+		'keyup.Enter': function(event) {
+			KAI.newState('meteo_page1');
+		},
+    'keyup.SoftRight': function(event) {
+			addPlace();
 		},
     'window.focus': function(event) {
       KAI.warmStart();
@@ -176,8 +355,10 @@ window.onload = function() {
 	KAI.init({
 	  options: {
 			appTitle: 						"Météo",
-			appVersion: 					"V1.0.0",
+			appVersion: 					"V1.0.1",
 			lang: 								'fr',
+			appTitleBackgroundColor:"#FF4B00",
+	    appTitleColor:          "#FFFFFF",
 			loadConfigFromSD: 		true,				// Compulsory : Boolean
 		  configFilePath: 			"/sdcard1/apps/meteo/config.json",	// Compulsory if loadConfigFromSD true
 		},
@@ -211,7 +392,6 @@ window.onload = function() {
 			// Here config is loaded
 			// We come back to the first day (today)
 			dayIndex = 0;
-			displayMeteoForSelectedCity();
 			KAI.newState('meteo_page1');
 	  }
 	});
